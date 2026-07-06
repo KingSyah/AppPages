2ccaeb243155991887f0e5ff472c8b1fc991169f
KingSyah <syah.syw@gmail.com>
2026-07-06 17:00:17 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'applauncher-v4';
+const CACHE_NAME = 'applauncher-v4.1';
 
 // Aset statis — aman di-cache lama (jarang berubah)
 // app.js disertakan di sini agar tombol langsung muncul dari cache (no delay)
